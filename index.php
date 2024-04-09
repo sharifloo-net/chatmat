9c03729677db1674a51c75aedadf6374f60ab905
sharifloo-net <sharifloo.net@gmail.com>
2024-04-09 16:59:20 +0330

    init project (With a simple design)

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="themes/default/css/style.css">
+    <title>ChatMat :)</title>
+</head>
+<body>
+<div id="container">
+    <div id="left-panel">
+        <div class="left-panel-item"><span></span>Public</div>
+        <div class="left-panel-item"><span></span>pc1</div>
+        <div class="left-panel-item"><span></span>pc2</div>
+        <div class="left-panel-item"><span></span>pc3</div>
+        <div class="left-panel-item"><span></span>pc4</div>
+        <div class="left-panel-item"><span></span>pc5</div>
+        <div class="left-panel-item"><span></span>pc6</div>
+        <div class="left-panel-item"><span></span>pc7</div>
+        <div class="left-panel-item"><span></span>pc8</div>
+        <div class="left-panel-item"><span></span>pc9</div>
+        <div class="left-panel-item"><span></span>pc10</div>
+    </div>
+    <div id="right-panel">
+        <div id="top-section">
+            <div class="right-section">
+                <div class="right-message-box">
+                    Hello This is a message test;
+                </div>
+            </div>
+            <div class="left-section">
+                <div class="left-message-box">
+                    Hello This is a message test;
+                </div>
+            </div>
+            <div class="right-section">
+                <div class="right-message-box">
+                    Hello This is a message test;
+                </div>
+            </div>
+            <div class="right-section">
+                <div class="right-message-box">
+                    Hello This is a message test;
+                </div>
+            </div>
+            <div class="left-section">
+                <div class="left-message-box">
+                    Hello This is a message test;
+                </div>
+            </div>
+        </div>
+        <div id="bottom-section">
+            <div id="text-box-container">
+                <input type="text" name="textBox" id="text-box" placeholder="Message"
+                       value="Hello This is a chat room"></input>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
