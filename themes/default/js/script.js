10a5d9fe45b907591747599c1fdcac94d7e8f485
sharifloo-net <sharifloo.net@gmail.com>
2024-04-11 13:25:52 +0330

    fix bug for when click on online/offline in left-panel

--- themes/default/js/script.js
+++ themes/default/js/script.js
@@ -5,6 +5,8 @@ let leftPanelItems = document.getElementsByClassName('left-panel-item'),
     leftOnline = document.getElementById('left-online');
 
 document.getElementById('left-panel').onclick = (e) => {
+    if (e.target.className === 'left-offline' || e.target.className === 'left-online')
+        return false;
     for (let i = leftPanelItems.length - 1; i >= 0; i--) {
         if (leftPanelItems[i].classList.contains('active')) {
             leftPanelItems[i].classList.remove('active');
