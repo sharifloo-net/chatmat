4e2a963f91cbf198eee0a0a29cee2fed405ca4f3
sharifloo-net <sharifloo.net@gmail.com>
2024-04-11 15:53:22 +0330

    add d-flex class for display: flex;

--- index.php
+++ index.php
@@ -12,81 +12,81 @@
 <body>
 <div id="container">
     <div id="left-panel">
-        <div class="left-panel-item active">
+        <div class="left-panel-item d-flex active">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>Public</span>
             </div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc1</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc2</span>
             </div>
             <div class="left-online">online</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc3</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc4</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc5</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc6</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc7</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc8</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc9</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
-        <div class="left-panel-item">
+        <div class="left-panel-item d-flex">
             <div class="left-panel-username">
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span>pc10</span>
             </div>
             <div class="left-offline">offline</div>
         </div>
     </div>
-    <div id="right-panel">
+    <div class="d-flex" id="right-panel">
         <div id="right-header-container">
-            <div id="right-header">
+            <div class="d-flex" id="right-header">
                 <div id="username-right-header">Public</div>
                 <div id="right-header-item-separator"></div>
                 <div id="left-offline">offline</div>
             </div>
         </div>
-        <div id="top-section">
+        <div class="d-flex" id="top-section">
             <div class="right-section">
                 <div class="message-box">
                     Hello This is a message test;
@@ -118,7 +118,7 @@
                 </div>
             </div>
         </div>
-        <div id="bottom-section">
+        <div class="d-flex" id="bottom-section">
             <div name="textBox" id="text-box" contenteditable="" placeholder="Message"
                  oninput="if(this.innerHTML.trim()==='<br>')this.innerHTML=''"></div>
         </div>
