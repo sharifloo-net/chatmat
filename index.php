04747f90b7d57c4bf6a178eabaf6191d592f3cbe
sharifloo-net <sharifloo.net@gmail.com>
2024-04-23 18:17:30 +0330

    Add icons to .context-menu

--- index.php
+++ index.php
@@ -204,20 +204,84 @@
     <div class="context-menu" id="right-panel-menu">
         <ul>
             <li>
-                <div>Test1</div>
+                <div>
+                    <svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" class="context-menu-icon">
+                        <path d="M0 0h48v48h-48z" fill="none"/>
+                        <path d="M32 2h-24c-2.21 0-4 1.79-4 4v28h4v-28h24v-4zm6 8h-22c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h22c2.21 0 4-1.79 4-4v-28c0-2.21-1.79-4-4-4zm0 32h-22v-28h22v28z"/>
+                    </svg>
+                    Copy
+                </div>
             </li>
             <li>
-                <div>Test2</div>
-            </li>
-            <li>
-                <div>Test3</div>
+                <div class="context-menu-delete">
+                    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                            <style>.cls-1 {
+                                    fill: none;
+                                }</style>
+                        </defs>
+                        <title/>
+                        <g data-name="Layer 2" id="Layer_2">
+                            <path d="M20,29H12a5,5,0,0,1-5-5V12a1,1,0,0,1,2,0V24a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V12a1,1,0,0,1,2,0V24A5,5,0,0,1,20,29Z"/>
+                            <path d="M26,9H6A1,1,0,0,1,6,7H26a1,1,0,0,1,0,2Z"/>
+                            <path d="M20,9H12a1,1,0,0,1-1-1V6a3,3,0,0,1,3-3h4a3,3,0,0,1,3,3V8A1,1,0,0,1,20,9ZM13,7h6V6a1,1,0,0,0-1-1H14a1,1,0,0,0-1,1Z"/>
+                            <path d="M14,23a1,1,0,0,1-1-1V15a1,1,0,0,1,2,0v7A1,1,0,0,1,14,23Z"/>
+                            <path d="M18,23a1,1,0,0,1-1-1V15a1,1,0,0,1,2,0v7A1,1,0,0,1,18,23Z"/>
+                        </g>
+                    </svg>
+                    Delete
+                </div>
             </li>
         </ul>
     </div>
     <div class="context-menu" id="left-panel-menu">
         <ul>
-            <li>
-                <div>Test3</div>
+            <li class="pin-context-menu">
+                <div>
+                    <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="context-menu-icon">
+                        <path d="M16.2425 2.93179L21.0682 7.75756C22.3955 9.08482 22.0324 11.3223 20.3535 12.1618L15.4826 14.5972C15.3073 14.6849 15.1732 14.8378 15.1092 15.0232L13.6699 19.1894C13.3684 20.0621 12.2574 20.318 11.6045 19.6652L8.50002 16.5606L4.06074 21H3L3.00008 19.9393L7.43936 15.5L4.33487 12.3955C3.682 11.7426 3.93791 10.6316 4.81061 10.3301L8.97688 8.89087C9.16223 8.82684 9.31512 8.69278 9.40281 8.51739L11.8382 3.64651C12.6777 1.96763 14.9152 1.60453 16.2425 2.93179ZM20.0076 8.81822L15.1818 3.99245C14.5785 3.38915 13.5614 3.5542 13.1799 4.31732L10.7445 9.1882C10.4814 9.71437 10.0227 10.1166 9.46666 10.3087L5.67812 11.6174L12.3826 18.3219L13.6914 14.5334C13.8835 13.9773 14.2857 13.5187 14.8118 13.2556L19.6827 10.8201C20.4458 10.4386 20.6109 9.42152 20.0076 8.81822Z"/>
+                    </svg>
+                    Pin
+                </div>
+            </li>
+            <li class="unpin-context-menu">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="context-menu-icon"
+                         x="0px" y="0px" viewBox="0 0 100 125" enable-background="new 0 0 100 100" xml:space="preserve"><g
+                                display="none">
+                            <line display="inline" fill="#000000" stroke="#E00C02" stroke-width="4"
+                                  stroke-miterlimit="10" x1="7" y1="93" x2="36" y2="64"/>
+                            <g display="inline">
+                                <path fill="#000000" stroke="#E00C02" stroke-width="4" stroke-miterlimit="10"
+                                      d="M62,59l1.74,20.012    c0.143,1.644-0.695,3.943-1.861,5.109L59,87"/>
+                            </g>
+                            <line display="inline" fill="#000000" stroke="#E00C02" stroke-width="4"
+                                  stroke-miterlimit="10" x1="81" y1="40" x2="62" y2="59"/>
+                            <g display="inline">
+                                <path fill="#000000" stroke="#E00C02" stroke-width="4" stroke-miterlimit="10"
+                                      d="M60,19v-4c0-1.65,0.9-3.9,2-5    s2.955-1.045,4.121,0.121l23.758,23.758C91.045,35.045,91.1,36.9,90,38s-3.35,2-5,2h-4"/>
+                            </g>
+                            <line display="inline" fill="#000000" stroke="#E00C02" stroke-width="4"
+                                  stroke-miterlimit="10" x1="41" y1="38" x2="60" y2="19"/>
+                            <g display="inline">
+                                <path fill="#000000" stroke="#E00C02" stroke-width="4" stroke-miterlimit="10"
+                                      d="M13,41l2.879-2.879    c1.167-1.166,3.466-2.004,5.11-1.861L41,38"/>
+                            </g>
+                            <polyline display="inline" fill="#000000" stroke="#E00C02" stroke-width="4"
+                                      stroke-miterlimit="10" points="16,38 13,41 59,87    62,84  "/>
+                            <line display="inline" fill="#000000" stroke="#E00C02" stroke-width="4"
+                                  stroke-miterlimit="10" x1="60" y1="19" x2="81" y2="40"/>
+                            <line display="inline" fill="#000000" stroke="#FFFFFF" stroke-width="8"
+                                  stroke-miterlimit="10" x1="13" y1="9" x2="91" y2="87"/>
+                            <line display="inline" fill="#000000" stroke="#E00C02" stroke-width="4"
+                                  stroke-miterlimit="10" x1="13" y1="9" x2="91" y2="87"/>
+                        </g>
+                        <g>
+                            <path d="M67.535,8.707c-1.95-1.949-5.066-2.004-6.949-0.121C59.112,10.06,58,12.816,58,15v3.172L42.914,33.258l2.828,2.828   L60,21.828L78.172,40L63.914,54.258l2.828,2.828L81.828,42H85c2.182,0,4.939-1.111,6.414-2.586   c0.926-0.925,1.414-2.171,1.375-3.509c-0.038-1.293-0.569-2.515-1.496-3.44L67.535,8.707z M88.586,36.586   C87.859,37.313,86.116,38,85,38h-3.172L62,18.172V15c0-1.116,0.687-2.859,1.414-3.586c0.317-0.316,0.91-0.261,1.293,0.121   l23.758,23.758c0.272,0.272,0.321,0.574,0.326,0.73C88.794,36.153,88.772,36.399,88.586,36.586z M63.406,85.406l0.008,0.008   l-0.121,0.121L59,89.828l-23-23L8.414,94.414l-2.828-2.828L33.172,64l-23-23l4.415-4.414l0.008,0.008   c1.607-1.518,4.386-2.518,6.567-2.326l12.54,1.09l4.397,4.397l-17.283-1.503c-1.066-0.084-2.771,0.533-3.522,1.283L15.829,41   L59,84.172l1.465-1.465c0.751-0.751,1.375-2.463,1.283-3.522l-1.503-17.283l4.398,4.397l1.09,12.54   C65.922,81.017,64.925,83.8,63.406,85.406z M14.414,7.586l78,78l-2.828,2.828l-78-78L14.414,7.586z"/>
+                        </g>
+                        </svg>
+                    unpin
+                </div>
             </li>
         </ul>
     </div>
